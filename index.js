2058ac7472c7166eaf84969e80d4d96916b08205
Pranith Gunda <pranith0527@yahoo.com>
2024-06-23 11:38:09 -0400

    Logo generation functionality completed. Exported Circle, Triangle and Square modules from shapes.js and imported in index.js to achieve logo generation

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// Import the required packages/modules
 const inquirer = require('inquirer')
 const fs = require('fs')
 const images = ['circle', 'triangle', 'square']
+const {Circle,Square,Triangle} = require('./lib/shapes')
+
+// Define variables at Global level
+let text = "";
+let textColor = "";
+let image = "";
+let imageColor = "";
+let svgLogoContent = "";
 
 inquirer
     .prompt([
@@ -10,6 +19,12 @@ inquirer
             name: "text"
         },
         {
+            type:"input",
+            message:"Please enter text color for the logo",
+            name:"textColor"
+
+        },
+        {
             type: "list",
             message: "Please select image for logo",
             name: "image",
@@ -17,10 +32,46 @@ inquirer
         },
         {
             type: "input",
-            message: "Please enter color for background",
-            name: "color"
+            message: "Please enter image color for the logo",
+            name: "imageColor"
         },
     ])
     .then((response) => {
-        console.log(response)
+        if(response.image === 'circle'){
+            let radius=40;
+            text=response.text;
+            textColor=response.textColor;
+            image=response.image;
+            imageColor=response.imageColor;
+            const circle=new Circle(radius,text,textColor,image,imageColor);
+            svgLogoContent = circle.render();
+            writeToFile('./examples/circle-logo.svg',svgLogoContent);
+        }
+        else if(response.image === 'triangle'){
+            text=response.text;
+            textColor=response.textColor;
+            image=response.image;
+            imageColor=response.imageColor;
+            const triangle=new Triangle(text,textColor,image,imageColor)
+            svgLogoContent=triangle.render();
+            writeToFile('./examples/triangle-logo.svg',svgLogoContent);
+        }
+        else if(response.image==='square'){
+            text=response.text;
+            textColor=response.textColor;
+            image=response.image;
+            imageColor=response.imageColor;
+            const square=new Square(text,textColor,image,imageColor)
+            svgLogoContent=square.render();
+            writeToFile('./examples/square-logo.svg',svgLogoContent);
+        }
+        else{
+            console.log('Please select valid image for logo generation')
+        }
     })
+
+    // function to generate file
+    function writeToFile(fileName,data){
+        fs.writeFile(fileName,data,(err)=>
+        err ? console.error(err):console.log(`${fileName} generated successfully`))
+    }
